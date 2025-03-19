182cceabf547614bc0437febfe2c0c5f48810352
Syed <abc@d.c>
2025-03-19 14:19:29 +0530

    Hide Sidebar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import "./App.css";
 import MainCssApp from "./components/css-experiment/MainCssApp";
+import Sidebar1 from "./components/hide-sidebar/Sidebar1";
 
 function App() {
-    return (
-        <>
-            <MainCssApp />
-        </>
-    );
+  return (
+    <>
+      {/* <MainCssApp /> */}
+      <Sidebar1 />
+    </>
+  );
 }
 
 export default App;
