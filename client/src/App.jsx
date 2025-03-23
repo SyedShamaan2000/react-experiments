3b1d61b5be08fedfbc4f4b2864cd9412f6eed3d8
Syed Shamaan <syedshamaan@Syeds-MacBook-Air.local>
2025-03-23 19:26:19 +0530

    Added Toggle Navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import MainCssApp from "./components/css-experiment/MainCssApp";
+import MainNav from "./components/toggle-navbar-component/MainNav";
 
 function App() {
     return (
         <>
-            <MainCssApp />
+            {/* <MainCssApp /> */}
+            <MainNav />
         </>
     );
 }
